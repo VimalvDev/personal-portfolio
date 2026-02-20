3609a2638d9d1a86a722e84a90aa720f32dfabe8
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2026-02-20 16:49:22 +0530

    before react bits

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -4,30 +4,30 @@ import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="flex justify-between w-full px-[2em] h-[4.5em] items-center z-50  fixed mix-blend-difference text-gray ">
+    <nav className="flex justify-between w-full px-[2em] h-[4.5em]  items-center z-50  fixed mix-blend-difference text-gray ">
       <div className="nav_logo text-[clamp(.5rem,1.1vw,1.4rem)] ">
         <Link to={"/"}>VIMAL.DEV</Link>
       </div>
 
       <div className="nav_link">
         <ul className="uppercase text-[clamp(.5rem,1vw,1.3rem)] flex gap-[1em] ">
-          <li>
-            <Link to={"/about"}>[ about me]</Link>
+          <li  >
+            <Link className=" py-[1.5em]" to={"/about"}>[ about me]</Link>
           </li>
           <li>
-            <Link to={"/projects"}>[ projects ]</Link>
+            <Link className=" py-[1.5em]"  to={"/projects"}>[ projects ]</Link>
           </li>
           <li>
-            <Link to={"/skills"}>[ skills ]</Link>
+            <Link className=" py-[1.5em]"  to={"/skills"}>[ skills ]</Link>
           </li>
           <li>
-            <Link to={"/connect"}>[ connect ]</Link>
+            <Link className=" py-[1.5em]"  to={"/connect"}>[ connect ]</Link>
           </li>
         </ul>
       </div>
 
       <div className="nav_talk">
-        <a href="mailto:hello@vimal.dev" className="flex items-center uppercase text-[clamp(.5rem,1vw,1.1rem)] ">
+        <a  href="mailto:hello@vimal.dev" className="flex py-[1.5em] items-center uppercase text-[clamp(.5rem,1vw,1.1rem)] ">
           [ let's talk <MdArrowOutward className="ml-1" />
         </a>
       </div>
--- src/pages/01-home/Home.jsx
+++ src/pages/01-home/Home.jsx
@@ -6,70 +6,94 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import gsap from "gsap";
 gsap.registerPlugin(ScrollTrigger);
 
-
-
 function Home() {
-  const containerPhilo = useRef(null)
-  const containerHero = useRef(null)
-  const container = useRef(null)
+  const containerPhilo = useRef(null);
+  const containerHero = useRef(null);
+  const container = useRef(null);
+  const bgPhiloContainer = useRef(null)
 
-  useGSAP(()=>{
-    const hero = containerHero.current
-    const philo = containerPhilo.current
-
-    ScrollTrigger.create({
-      trigger: philo,
-      start: "top 40%",
-      onEnter:()=>{
-        gsap.to(hero,{
-          backgroundColor: "var(--color-light-black)",
-          duration:.8,
-        })
-        gsap.to(philo,{
-          backgroundColor: "var(--color-light-black)",
-          duration:.8,
-        })
-      },
-      onLeaveBack: ()=>{
-        gsap.to(hero,{
-          backgroundColor: "var(--color-very-light)",
-          duration: .8,
-        })
-        gsap.to(philo,{
-          backgroundColor: "var(--color-very-light)",
-          duration: .8,
-        })
-      }
-    })
+  useGSAP(
+    () => {
+      const hero = containerHero.current;
+      const philo = containerPhilo.current;
+      const bgPhilo = bgPhiloContainer.current
 
 
-  },{scope: container})
- 
+      ScrollTrigger.create({
+        trigger: bgPhilo,
+        start: "top 40%",
+        onEnter: () => {
+          gsap.to(hero, {
+            backgroundColor: "var(--color-light-black)",
+            duration: 0.8,
+          });
+          gsap.to(bgPhilo, {
+            backgroundColor: "var(--color-light-black)",
+            duration: 0.8,
+          });
+        },
+        onLeaveBack: () => {
+          gsap.to(hero, {
+            backgroundColor: "var(--color-very-light)",
+            duration: 0.8,
+          });
+          gsap.to(bgPhilo, {
+            backgroundColor: "var(--color-very-light)",
+            duration: 0.8,
+          });
+        },
+      });
+
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: philo,
+            start: "top 90%",
+            end: "bottom top",
+            scrub: true,
+          },
+        })
+        .fromTo(
+          philo,
+          {
+            opacity: 0,
+            duration: .6
+          },
+          { opacity: 1, ease: "none" },
+        )
+        .to(philo, { opacity:0, ease: "none" });
+    },
+    { scope: container },
+  );
+
   return (
-    <div ref={container} >
-      <div ref={containerHero} className="hero_page flex  min-h-screen w-full flex-col px-[2em]  justify-between py-[1em]">
+    <div ref={container}>
+      <div
+        ref={containerHero}
+        className="hero_page flex  min-h-screen w-full flex-col px-[2em]  justify-between py-[1em]"
+      >
         <HeroHeading />
         <HeroBottomText />
       </div>
 
-      <div ref={containerPhilo} className="philosophy_section min-h-[150vh] text-white text-center flex flex-col items-center justify-center ">
-        <div className="approach_text mix-blend-difference ">
-          <span className="uppercase text-gray text-[clamp(.5rem,1vw,1rem)] ">
-            [ my thinking]
-          </span>
+      <div ref={bgPhiloContainer} className="philosophy_section min-h-screen text-white text-center flex flex-col items-center justify-center ">
+        <div ref={containerPhilo} className="philo_container">
+          <div className="approach_text mix-blend-difference ">
+            <span className="uppercase text-gray text-[clamp(.5rem,1vw,1rem)] ">
+              [ my thinking]
+            </span>
+          </div>
+          <h2 className=" uppercase text-white mix-blend-difference text-[clamp(1rem,5vw,10rem)] font-inter font-bold leading-tight ">
+            design is not decoration. it is clarity, motion, and experience.
+          </h2>
+          <p>
+            i combine design thinking with technical precision to create digital
+            experiences that feel structured and alive
+          </p>
         </div>
-        <h2 className=" uppercase text-very-light mix-blend-difference text-[clamp(1rem,5vw,10rem)] font-inter font-bold leading-tight ">
-          design is not decoration. it is clarity, motion, and experience.
-        </h2>
-        <p>
-          i combine design thinking with technical precision to create digital
-          experiences that feel structured and alive
-        </p>
       </div>
 
-      <div className="h-screen">
-        hi
-      </div>
+      <div className="h-screen">hi</div>
     </div>
   );
 }
