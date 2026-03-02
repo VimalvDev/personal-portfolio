ee6a22b4764705c9b028a3e28f7f85fe9296f0da
Vimal | Frontend <165493811+VimalvDev@users.noreply.github.com>
2026-03-03 02:36:11 +0530

    deployed on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import Nav from "./components/common/Nav";
 import Routing from "./routes/Routing";
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import ReactLenis from "lenis/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -37,6 +37,7 @@ function App() {
 
         <Nav />
         <Routing />
+        <SpeedInsights />
       </ReactLenis>
     </PageTransitionProvider>
   );
